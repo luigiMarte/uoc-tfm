65e148028381ca2861b4c67d3827d02843f00624
Luis Martinez <luis.martinez@healios.io>
2023-01-14 10:08:52 -0300

    change route component

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import CompleteProfile from "../views/CompleteProfile.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -71,7 +72,8 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import("../views/completeProfile.vue"),
+      //component: () => import("../views/completeProfile.vue"),
+      component: CompleteProfile,
     },
     {
       path: "/edit",
