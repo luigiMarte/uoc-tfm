213af1768431d2ea5440f8be48149b50a4d3646d
Luis Martinez <luis.martinez@healios.io>
2023-01-14 10:24:24 -0300

    index route correction

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
-import CompleteProfile from "../views/CompleteProfile.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -72,8 +71,7 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      //component: () => import("../views/completeProfile.vue"),
-      component: CompleteProfile,
+      component: () => import("../views/CompleteProfile.vue"),
     },
     {
       path: "/edit",
