ad4210f1d4594ab841fc2f3563eff93197dc5b9c
Luis Martinez <luis.martinez@healios.io>
2023-01-14 10:36:39 -0300

    remove conflicted images

--- src/views/LandingView.vue
+++ src/views/LandingView.vue
@@ -1,5 +1,5 @@
 <template>
-  <img src="@/assets/img/landing.jpg" alt="" />
+  <!-- <img src="@/assets/img/landing.jpg" alt="" /> -->
   <b-container class="mt-4 m-bot-50">
     <div>
       <div></div>
@@ -194,7 +194,7 @@ img {
 
 /* start home */
 #home {
-  background: url("../images/home-bg.jpg") no-repeat;
+  //background: url("../images/home-bg.jpg") no-repeat;
   background-size: cover;
   color: #fff;
   margin-top: 70px;
@@ -347,7 +347,7 @@ img {
 
 /* start contact */
 #contact {
-  background: url("../images/contact-bg.jpg") no-repeat;
+  //background: url("../images/contact-bg.jpg") no-repeat;
   background-size: cover;
   color: #fff;
 }
